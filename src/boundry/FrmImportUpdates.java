1c9e9bcf77dde2d7431416c45d6489d266e2a3ca
yamitehr <yamitehrlich@gmail.com>
2021-12-31 17:43:35 +0200

    no message

--- src/boundry/FrmImportUpdates.java
+++ src/boundry/FrmImportUpdates.java
@@ -1,6 +1,7 @@
 package boundry;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,8 @@ import java.util.Map;
 import control.ImportControl;
 import entity.Flight;
 import entity.FlightTicket;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -25,7 +28,6 @@ public class FrmImportUpdates
 	
 	@FXML
 	Button importJson;
-	
 	
 	@FXML
 	Button btnSend;
@@ -53,14 +55,20 @@ public class FrmImportUpdates
 	@FXML
 	public void onButtonClick(MouseEvent event)
 	{
+		ArrayList<Flight> flights = new ArrayList<Flight>();
 		allFlightsMap = ImportControl.getAllUpdatedFlightsAndTickets();
 		for(Map.Entry<Flight, HashMap<SeatClass, List<FlightTicket>>> entry : allFlightsMap.entrySet()){
-			affectedFlights.getItems().add(entry.getKey());
+			flights.add(entry.getKey());
 		}
+		ObservableList<Flight> flt = FXCollections.observableList(flights);
+		affectedFlights.setItems(FXCollections.observableArrayList(flt));
 		
+		ArrayList<FlightTicket> flightTickets = new ArrayList<FlightTicket>();
 		for(FlightTicket ticket :  ImportControl.getCustmersCantSeat()) {
-			affectedTickets.getItems().add(ticket);
+			flightTickets.add(ticket);
 		}
+		ObservableList<FlightTicket> flttk = FXCollections.observableList(flightTickets);
+		affectedTickets.setItems(FXCollections.observableArrayList(flttk));
 			
 	}
 }
