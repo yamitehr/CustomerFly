0f849a13b35e83c3afc254b33c9cf9a34dc791ac
yamitehr <yamitehrlich@gmail.com>
2021-12-31 23:45:02 +0200

    notify

--- src/boundry/FrmImportUpdates.java
+++ src/boundry/FrmImportUpdates.java
@@ -1,16 +1,21 @@
 package boundry;
 
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import control.AlternativeFlightsLogic;
 import control.ImportControl;
+import control.ReportsLogic;
 import entity.Flight;
 import entity.FlightTicket;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -25,6 +30,8 @@ public class FrmImportUpdates
 	ListView<Flight> affectedFlights;
 	@FXML
 	ListView<FlightTicket> affectedTickets;
+	private ArrayList<Flight> flights = new ArrayList<Flight>();
+	private ArrayList<FlightTicket> flightTickets = new ArrayList<FlightTicket>();
 	
 	@FXML
 	Button importJson;
@@ -39,23 +46,22 @@ public class FrmImportUpdates
 	Label messageLbl;
 	
 	@FXML
-	Button btnMail;
+	Button btnReport;
 	
 	private HashMap<Flight, HashMap<SeatClass, List<FlightTicket>>> allFlightsMap;
+	AlternativeFlightsLogic alterFlightLogic = AlternativeFlightsLogic.getInstance();
 
 	
 	
 	@FXML
 	public void initialize() 
 	{
-		btnMail.setVisible(false);
 	}
 	
 	//this method will happen after pressing the import JSON button
 	@FXML
 	public void onButtonClick(MouseEvent event)
 	{
-		ArrayList<Flight> flights = new ArrayList<Flight>();
 		allFlightsMap = ImportControl.getAllUpdatedFlightsAndTickets();
 		for(Map.Entry<Flight, HashMap<SeatClass, List<FlightTicket>>> entry : allFlightsMap.entrySet()){
 			flights.add(entry.getKey());
@@ -63,12 +69,31 @@ public class FrmImportUpdates
 		ObservableList<Flight> flt = FXCollections.observableArrayList(flights);
 		affectedFlights.setItems(FXCollections.observableArrayList(flt));
 		
-		ArrayList<FlightTicket> flightTickets = new ArrayList<FlightTicket>();
 		for(FlightTicket ticket :  ImportControl.getCustmersCantSeat()) {
 			flightTickets.add(ticket);
 		}
 		ObservableList<FlightTicket> flttk = FXCollections.observableArrayList(flightTickets);
 		affectedTickets.setItems(FXCollections.observableArrayList(flttk));
 			
+	}
+	
+	@FXML
+	public void notify(Event event) {
+		if(affectedTickets.getSelectionModel().getSelectedItem() != null) {
+			FlightTicket flightTicket = affectedTickets.getSelectionModel().getSelectedItem();
+			String selectedFlightID = flightTicket.getFlight().getFlightID();
+			Flight currentFlight= null;
+			for(Flight flight : flights) {
+				if(flight.getFlightID().equals(selectedFlightID)) {
+					currentFlight = flight;
+				}
+			}
+			if(currentFlight != null) {
+				alterFlightLogic.getAlternativeRecomandedFlights(currentFlight.getFlightID(), currentFlight.getDepartureDateTime(),
+						currentFlight.getDepartureAirport().getCity(),
+						currentFlight.getDestinationAirport().getCity(), currentFlight.getDepartureAirport().getCountry(),
+						currentFlight.getDestinationAirport().getCountry(), flightTicket.getSeatClass());
+			}
+		}
 	}
 }
